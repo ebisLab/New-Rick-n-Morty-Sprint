5187eca45e2f4046bf33d9a72d17567c8e264eab
ebisLab <ebaiden@techie.com>
2020-01-31 13:05:53 -0500

    Welcome and Character List page routers set

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
-import CharacterCard from './CharacterCard'
+import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
   const [data, setData ]= useState([]);
