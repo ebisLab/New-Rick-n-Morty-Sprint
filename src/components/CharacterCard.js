b3d513765db19d0772e834d4ef60446f4b584c56
ebisLab <ebaiden@techie.com>
2020-01-31 12:23:07 -0500

    character information is being rendered

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,16 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+
+  console.log(props.chars)
+  return (
+    <div>
+    <h1>{props.chars.name}</h1>
+    <img src={props.chars.image}/>
+    <p>{props.chars.status}</p>
+    <p>{props.chars.species}</p>
+
+  <span>todo: character</span>
+  </div>
+  )
 }
